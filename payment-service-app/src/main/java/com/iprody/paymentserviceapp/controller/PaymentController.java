2da5c76f64495445aaa7fcf3b59bfed0df0a95d6
A&V <alexteplykh@rambler.ru>
2025-07-08 23:05:35 +0300

    create controller PaymentController

---
+++ payment-service-app/src/main/java/com/iprody/paymentserviceapp/controller/PaymentController.java
@@ -0,0 +1,33 @@
+package com.iprody.paymentserviceapp.controller;
+
+import com.iprody.paymentserviceapp.model.Payment;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/payments")
+public class PaymentController {
+    private final Map<Long, Payment> paymentMap = Map.of(
+            1L, new Payment(1L, 23.5),
+            2L, new Payment(2L, 107.4),
+            3L, new Payment(3L, 41.8),
+            4L, new Payment(4L, 94.7),
+            5L, new Payment(5L, 36.7)
+    );
+
+    @GetMapping("/{id}")
+    public Payment getPayment(@PathVariable Long id) {
+        return paymentMap.get(id);
+    }
+
+    @GetMapping
+    public List<Payment> getAllPayment() {
+        return paymentMap.values().stream().toList();
+    }
+}
