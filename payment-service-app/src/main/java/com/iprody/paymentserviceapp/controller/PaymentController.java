e53394acececb5c6b20fa57b9a5a9da6656c2462
A&V <alexteplykh@rambler.ru>
2025-07-23 23:45:11 +0300

    refactor PaymentController

--- payment-service-app/src/main/java/com/iprody/paymentserviceapp/controller/PaymentController.java
+++ payment-service-app/src/main/java/com/iprody/paymentserviceapp/controller/PaymentController.java
@@ -6,19 +6,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/payments")
 public class PaymentController {
-    private final Map<Long, Payment> paymentMap = Map.of(
-        1L, new Payment(1L, 23.5),
-        2L, new Payment(2L, 107.4),
-        3L, new Payment(3L, 41.8),
-        4L, new Payment(4L, 94.7),
-        5L, new Payment(5L, 36.7)
-    );
+    private final Map<Long, Payment> paymentMap = new HashMap<>();
 
     @GetMapping("/{id}")
     public Payment getPayment(@PathVariable Long id) {
--- payment-service-app/src/main/java/com/iprody/paymentserviceapp/persistence/entity/Payment.java
+++ payment-service-app/src/main/java/com/iprody/paymentserviceapp/persistence/entity/Payment.java
@@ -1,11 +1,10 @@
 package com.iprody.paymentserviceapp.persistence.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
 @Entity
@@ -13,18 +12,34 @@ import java.util.UUID;
 public class Payment {
 
     @Id
-    private UUID id;
-    private UUID inquiryRefId;
-    private BigDecimal amount;
-    private String currency;
-    private UUID transactionReadId;
-    private PaymentStatus status;
-    private String note;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
+    @Column(nullable = false, unique = true)
+    private UUID guid;
 
+    @Column(nullable = false, name = "inquiry_ref_id")
+    private UUID inquiryRefId;
+
+    @Column(nullable = false, precision = 5, scale = 2)
+    private BigDecimal amount;
+
+    @Column(nullable = false, length = 3)
+    private String currency;
+
+    @Column(name = "transaction_ref_id")
+    private UUID transactionRefId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PaymentStatus status;
+
+    @Column(columnDefinition = "text")
+    private String note;
+
+    @Column(name = "created_at", nullable = false)
+    private OffsetDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private OffsetDateTime updatedAt;
 
     public Payment() {
-
     }
 }
