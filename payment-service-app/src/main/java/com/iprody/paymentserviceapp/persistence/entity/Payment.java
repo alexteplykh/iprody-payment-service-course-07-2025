aea8d73beb9001c46378c158ceade94d624fe15f
A&V <alexteplykh@rambler.ru>
2025-07-30 23:19:56 +0300

    apply lombok annotation to Payment

--- payment-service-app/src/main/java/com/iprody/paymentserviceapp/persistence/entity/Payment.java
+++ payment-service-app/src/main/java/com/iprody/paymentserviceapp/persistence/entity/Payment.java
@@ -1,14 +1,19 @@
 package com.iprody.paymentserviceapp.persistence.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
 @Entity
 @Table(name = "payments")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Payment {
 
     @Id
@@ -39,79 +44,4 @@ public class Payment {
 
     @Column(name = "updated_at", nullable = false)
     private OffsetDateTime updatedAt;
-
-    public Payment() {
-    }
-
-    public UUID getGuid() {
-        return guid;
-    }
-
-    public void setGuid(UUID guid) {
-        this.guid = guid;
-    }
-
-    public UUID getInquiryRefId() {
-        return inquiryRefId;
-    }
-
-    public void setInquiryRefId(UUID inquiryRefId) {
-        this.inquiryRefId = inquiryRefId;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public String getCurrency() {
-        return currency;
-    }
-
-    public void setCurrency(String currency) {
-        this.currency = currency;
-    }
-
-    public UUID getTransactionRefId() {
-        return transactionRefId;
-    }
-
-    public void setTransactionRefId(UUID transactionRefId) {
-        this.transactionRefId = transactionRefId;
-    }
-
-    public PaymentStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(PaymentStatus status) {
-        this.status = status;
-    }
-
-    public String getNote() {
-        return note;
-    }
-
-    public void setNote(String note) {
-        this.note = note;
-    }
-
-    public OffsetDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(OffsetDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public OffsetDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(OffsetDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
 }
