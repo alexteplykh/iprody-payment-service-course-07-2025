e836ccb0037c7e922e14f60fcc3edf12c0d3dc0d
A&V <alexteplykh@rambler.ru>
2025-08-01 00:33:32 +0300

    create PaymentService

---
+++ payment-service-app/src/main/java/com/iprody/paymentserviceapp/service/PaymentService.java
@@ -0,0 +1,39 @@
+package com.iprody.paymentserviceapp.service;
+
+import com.iprody.paymentserviceapp.controller.PaymentFilterDto;
+import com.iprody.paymentserviceapp.dto.PaymentDto;
+import com.iprody.paymentserviceapp.mapper.PaymentMapper;
+import com.iprody.paymentserviceapp.persistence.PaymentFilterFactory;
+import com.iprody.paymentserviceapp.persistence.PaymentRepository;
+import com.iprody.paymentserviceapp.persistence.entity.Payment;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class PaymentService implements PaymentServiceInterface{
+    private final PaymentRepository paymentRepository;
+    private final PaymentMapper paymentMapper;
+
+    public PaymentService(PaymentRepository paymentRepository,
+                          PaymentMapper paymentMapper) {
+        this.paymentRepository = paymentRepository;
+        this.paymentMapper = paymentMapper;
+    }
+
+    public PaymentDto get(UUID id) {
+        return paymentRepository.findById(id)
+                .map(paymentMapper::toDto)
+                .orElseThrow(() -> new EntityNotFoundException("Платеж не найден: " + id));
+    }
+
+    public Page<PaymentDto> search(PaymentFilterDto filter, Pageable pageable) {
+        Specification<Payment> spec = PaymentFilterFactory.fromFilter(filter);
+        Page<Payment> page = paymentRepository.findAll(spec, pageable);
+        return page.map(paymentMapper::toDto);
+    }
+}
