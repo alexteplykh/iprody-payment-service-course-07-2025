ff388b49ec235564f9d5876e56bab09ca6164078
A&V <alexteplykh@rambler.ru>
2025-08-07 22:53:39 +0300

    add methods create, delete, update in PaymentController

--- payment-service-app/src/main/java/com/iprody/paymentserviceapp/controller/PaymentController.java
+++ payment-service-app/src/main/java/com/iprody/paymentserviceapp/controller/PaymentController.java
@@ -4,6 +4,7 @@ import com.iprody.paymentserviceapp.dto.PaymentDto;
 import com.iprody.paymentserviceapp.service.PaymentServiceInterface;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -15,11 +16,19 @@ public class PaymentController {
 
     private final PaymentServiceInterface paymentService;
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public PaymentDto create(@RequestBody PaymentDto dto) {
+        return paymentService.create(dto);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{guid}")
     public PaymentDto getPayment(@PathVariable UUID guid) {
         return paymentService.get(guid);
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/search")
     public Page<PaymentDto> getAllPayment(@ModelAttribute PaymentFilterDto paymentFilter,
                                           @RequestParam(defaultValue = "1") int page,
@@ -28,5 +37,16 @@ public class PaymentController {
                                           @RequestParam(defaultValue = "desc") String direction) {
 
         return paymentService.search(paymentFilter, page, size, sortedBy, direction);
+    }
+
+    @PutMapping("/{id}")
+    public PaymentDto update(@PathVariable UUID id, @RequestBody PaymentDto dto) {
+        return paymentService.update(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable UUID id) {
+        paymentService.delete(id);
     }
 }
