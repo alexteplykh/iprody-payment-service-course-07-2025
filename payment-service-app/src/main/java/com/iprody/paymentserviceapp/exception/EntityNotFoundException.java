51fd004692bcd947b931334db8da24cdccaff6d2
A&V <alexteplykh@rambler.ru>
2025-08-15 00:40:52 +0300

    add argument id, operation in EntityNotFoundException

--- payment-service-app/src/main/java/com/iprody/paymentserviceapp/exception/EntityNotFoundException.java
+++ payment-service-app/src/main/java/com/iprody/paymentserviceapp/exception/EntityNotFoundException.java
@@ -1,11 +1,22 @@
 package com.iprody.paymentserviceapp.exception;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.util.UUID;
+
+@Getter
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class EntityNotFoundException extends RuntimeException{
-    public EntityNotFoundException(String message) {
+    private UUID id;
+    private String operation;
+
+    public EntityNotFoundException(UUID id, String op, String message) {
         super(message);
+        this.id = id;
+        this.operation = op;
     }
+
+
 }
