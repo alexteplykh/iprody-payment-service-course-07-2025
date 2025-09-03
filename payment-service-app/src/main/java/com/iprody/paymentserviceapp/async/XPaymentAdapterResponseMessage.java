a275a91973aae147717c0cfc3ca48dd3436416bf
A&V <alexteplykh@rambler.ru>
2025-09-03 23:50:17 +0300

    refactor XPaymentAdapterResponseMessage

--- payment-service-app/src/main/java/com/iprody/paymentserviceapp/async/XPaymentAdapterResponseMessage.java
+++ payment-service-app/src/main/java/com/iprody/paymentserviceapp/async/XPaymentAdapterResponseMessage.java
@@ -1,44 +1,21 @@
 package com.iprody.paymentserviceapp.async;
 
+import com.iprody.paymentserviceapp.persistence.entity.Currency;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
-import java.util.Currency;
 import java.util.UUID;
 
+@Getter
+@Setter
 public class XPaymentAdapterResponseMessage implements Message {
-
-    @Setter
-    private UUID messageGuid;
-
-    @Getter
-    @Setter
+    private UUID messageId;
     private UUID paymentGuid;
-
-    @Getter
-    @Setter
     private BigDecimal amount;
-
-    @Getter
-    @Setter
     private Currency currency;
-
-    @Getter
-    @Setter
     private UUID transactionRefId;
-
-    @Getter
-    @Setter
     private XPaymentAdapterStatus status;
-
-    @Getter
-    @Setter
     private OffsetDateTime occurredAt;
-
-    @Override
-    public UUID getMessageId() {
-        return messageGuid;
-    }
 }
