a4b464caae18fe6d893e535d8791d4cf5920668d
A&V <alexteplykh@rambler.ru>
2025-09-09 00:39:47 +0300

    create KafkaXPaymentAdapterRequestSender

---
+++ payment-service-app/src/main/java/com/iprody/paymentserviceapp/async/kafka/KafkaXPaymentAdapterRequestSender.java
@@ -0,0 +1,30 @@
+package com.iprody.paymentserviceapp.async.kafka;
+
+import com.iprody.paymentserviceapp.async.AsyncSender;
+import com.iprody.paymentserviceapp.async.XPaymentAdapterRequestMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+
+public class KafkaXPaymentAdapterRequestSender implements AsyncSender<XPaymentAdapterRequestMessage> {
+    private static final Logger log = LoggerFactory.getLogger(KafkaXPaymentAdapterRequestSender.class);
+
+    private final KafkaTemplate<String, XPaymentAdapterRequestMessage> template;
+    private final String topic;
+
+    public KafkaXPaymentAdapterRequestSender(KafkaTemplate<String, XPaymentAdapterRequestMessage> template,
+                                             @Value("${app.kafka.topics.xpayment-adapter.request:xpayment-adapter.requests}") String topic) {
+        this.template = template;
+        this.topic = topic;
+    }
+
+    @Override
+    public void send(XPaymentAdapterRequestMessage msg) {
+        String key = msg.getPaymentGuid().toString();
+
+        log.info("Sending XPayment Adapter request: guid={}, amount={}, currency={} -> topic={}",
+                msg.getPaymentGuid(), msg.getAmount(), msg.getCurrency(), topic);
+        template.send(topic, key, msg);
+    }
+}
